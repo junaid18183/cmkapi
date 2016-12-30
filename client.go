5b95fe4dc4c7f5444cfac43e11adc0ff5691f468
Juned Memon <junaid18183@gmail.com>
2016-12-30 14:27:16 +0000

    Fixed few bugs

--- client.go
+++ client.go
@@ -1,40 +1,18 @@
 package cmkapi
 
-/*import (
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
-)*/
 import ( "net/http" )
 
-type Session struct {
-        Username           string
-        Password           string
-        BaseURL            string
-        AllowUnverifiedSSL bool
+type Client struct {
+        User     string
+        Password   string
+        Host    string
+        Sitename string
         httpClient         *http.Client
 }
 
-// func NewSession ...
-func NewSession(username, password, url string,allowUnverifiedSSL bool) (*Session, error) {
-        return &Session{username, password, url,allowUnverifiedSSL, nil}, nil
+
+// func NewClient
+func NewClient(user, password, host,sitename string) (*Client, error) {
+        return &Client{user, password,host,sitename, nil}, nil
 }
 
-/*func (session *Session) Connect() error {
-	var body []byte
-	var response *http.Response
-	var request *http.Request
-	request, err := http.NewRequest("GET", session.BaseURL, nil)
-    	if err == nil {
-        	request.Header.Add("Content-Type", "application/json")
-        	response, err = (&http.Client{}).Do(request)
-        	defer response.Body.Close()
-        	body, err = ioutil.ReadAll(response.Body)
-        	fmt.Printf("%s", body)
-    	} else {
-        	log.Fatalf("ERROR: %s", err)
-    	}
-	return nil
-}
-*/
--- hosts.go
+++ hosts.go
@@ -7,24 +7,55 @@ import (
     "net/http"
 )
 
+type Host struct {
+        Hostname string
+        Folder   string
+        Alias    string
+        TAG_Agent string
+        TAG_criticality string
+        IPADDRESS string
+}
 
 
-func (session *Session) GetHosts() error {
-	var body []byte
-	var response *http.Response
-	var request *http.Request
-	fullurl := session.BaseURL + "?action=get_all_hosts&_username=" + session.Username + "&_secret=" + session.Password
-	//fmt.Printf("%s", fullurl)
+func (h *Host) Id() string {
+        return "id-" + h.Hostname + "!"
+}
+
+func (c *Client) CreateHost(h *Host) error {
+        return nil
+}
+
+
+
+func (c *Client) ReadHost() error {
+//func (c *Client) ReadHost(h Host) error {
+//	var body []byte
+//	var response *http.Response
+//	var request *http.Request
+	baseurl := "http://" + c.Host + "/" + c.Sitename + "/check_mk/webapi.py"
+	action := "?action=get_all_hosts"
+	//action := "?action=get_host"
+	credentails := "&_username=" + c.User + "&_secret=" + c.Password
+	fullurl := baseurl + action + credentails 
 
 	request, err := http.NewRequest("GET", fullurl, nil)
-    	if err == nil {
-        	request.Header.Add("Content-Type", "application/json")
-        	response, err = (&http.Client{}).Do(request)
-        	defer response.Body.Close()
-        	body, err = ioutil.ReadAll(response.Body)
-        	fmt.Printf("%s", body)
-    	} else {
-        	log.Fatalf("ERROR: %s", err)
-    	}
+	if err == nil {
+                request.Header.Add("Content-Type", "application/json")
+                response, err1 := (&http.Client{}).Do(request)
+		if err1 == nil {
+                	defer response.Body.Close()
+                	body, err2 := ioutil.ReadAll(response.Body)
+			if err2 == nil {
+                		fmt.Printf("%s", body)
+			} else {
+			log.Fatalf("ERROR: %s", err2)
+			}
+		} else { 
+		log.Fatalf("ERROR: %s", err1)
+        }
+		
+        } else {
+                log.Fatalf("ERROR: %s", err)
+        }
 	return nil
 }

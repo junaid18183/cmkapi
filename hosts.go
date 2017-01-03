2e034e34d04cb5c8f5d6a9746cd170a5ee3314cb
Juned Memon <junaid18183@gmail.com>
2017-01-03 11:05:33 +0000

    Added Create Host Function

--- hosts.go
+++ hosts.go
@@ -4,30 +4,47 @@ import (
     "fmt"
     "strings"
     "encoding/json"
+    "errors"
 )
-
-type Host struct {
-	Attributes
-        Hostname string
-        Folder   string
-}
-
-type Attributes struct {
-	Alias string
-	Agent string
-	IPADDRESS string
-	Criticality string
-}
-
-
+//#-------------------------------------------------------------------------------------------------------------------------------------------
 func (h *Host) Id() string {
         return "id-" + h.Hostname + "!"
 }
-
-func (c *Client) CreateHost(h *Host) error {
-        return nil
+//#-------------------------------------------------------------------------------------------------------------------------------------------
+func (c *Client) CreateHost(hostname, folder, alias,tag_agent,tag_criticality,ipaddress string) error {
+	var result StructPutResult
+	host := &Host{Attributes {alias,tag_agent,tag_criticality,ipaddress},hostname, folder }
+	h, marshal_err := json.Marshal(host)
+    	if marshal_err != nil {
+        	fmt.Printf("Error Creating the Host Struct: %s\n", marshal_err)
+        	return marshal_err
+    	}
+	s := "request=" + string(h)
+	body := strings.NewReader(s)
+	resp_body, resp_err := c.NewAPIRequest("POST","add_host",body)
+        if resp_err != nil {
+		fmt.Printf("API Request for add_host failed. Error: %s\n", resp_err)
+		return resp_err
+	}
+	resp_unmarshal_err := json.Unmarshal(resp_body, &result)
+        if resp_unmarshal_err != nil {
+		fmt.Printf("Error Decoding the API response. Error: %s\n", resp_unmarshal_err)
+		return resp_unmarshal_err
+	}
+        resp_code := result.ResultCode
+        if resp_code != 0 {
+                fmt.Printf("API Response Failed. Error:%s\n", result.Result)
+                return errors.New("API Response Failed")
+	}
+	//Add host is sucssfull , now Call activate_changes
+        status:=c.ActivateChanges()
+        if status != nil {
+		fmt.Printf("Activate Change got Failed, %s\n", status)
+                return errors.New("Activate Change got Failed, status")
+	}                          
+        fmt.Printf("Host %s is added Successfully \n",host.Hostname)
+	return nil
 }
-
 //#-------------------------------------------------------------------------------------------------------------------------------------------
 func (c *Client) ReadALLHost() error {
 	resp_body, resp_err := c.NewAPIRequest("GET","get_all_hosts",nil)
@@ -36,61 +53,72 @@ func (c *Client) ReadALLHost() error {
         }
         fmt.Printf("%s",resp_body)
         return nil
-
 }
 //#-------------------------------------------------------------------------------------------------------------------------------------------
 func (c *Client) ReadHost(host string) error {
         var hostdetail StructGetHostResult
-        s := "request={\"hostname\": \"" + host + "\"}"
+	s := "request={\"hostname\": \"" + host + "\"}"
         body := strings.NewReader(s)
-	resp_body, resp_err := c.NewAPIRequest("POST","get_host",body)
-	if resp_err != nil {
-		return resp_err
-	}
-	err := json.Unmarshal(resp_body, &hostdetail)
-        if err == nil {
-        fmt.Printf("%s", hostdetail.Result.Hostname)
+        resp_body, resp_err := c.NewAPIRequest("POST","get_host",body)
+        if resp_err != nil {
+                fmt.Printf("API Request for get_host failed. Error: %s\n", resp_err)
+                return resp_err
         }
+        resp_unmarshal_err := json.Unmarshal(resp_body, &hostdetail)
+        if resp_unmarshal_err != nil {
+                fmt.Printf("Error Decoding the API response. Error: %s\n", resp_unmarshal_err)
+                return resp_unmarshal_err
+        }
+        fmt.Printf("%s \n", hostdetail.Result.Hostname)
         return nil
 }
 //#-------------------------------------------------------------------------------------------------------------------------------------------
 func (c *Client) DeleteHost(host string) error {
-	var result StructPutResult
-        s := "request={\"hostname\": \"" + host + "\"}"
+        var result StructPutResult
+		s := "request={\"hostname\": \"" + host + "\"}"
         body := strings.NewReader(s)
-	resp_body, resp_err := c.NewAPIRequest("POST","delete_host",body)
-	if resp_err == nil {
-		err := json.Unmarshal(resp_body, &result)
-		if err == nil {
-			resp_code := result.ResultCode
-			if resp_code == 0 {
-				//Delete is sucssfull , now Call activate_changes
-				status:=c.ActivateChanges()
-				if status == nil {
-					fmt.Printf("%s is Deleted Successfully",host)
-				}
-				return nil
-        		}
-		} 
-		return err  
-	}
-	return resp_err
+        resp_body, resp_err := c.NewAPIRequest("POST","delete_host",body)
+        if resp_err != nil {
+                fmt.Printf("API Request for delete_host failed. Error: %s\n", resp_err)
+                return resp_err
+        }
+        resp_unmarshal_err := json.Unmarshal(resp_body, &result)
+        if resp_unmarshal_err != nil {
+                fmt.Printf("Error Decoding the API response. Error: %s\n", resp_unmarshal_err)
+                return resp_unmarshal_err
+        }
+        resp_code := result.ResultCode
+        if resp_code != 0 {
+                fmt.Printf("API Response Failed. Error:%s\n", result.Result)
+                return errors.New("API Response Failed")
+        }
+        //Delete host is sucssfull , now Call activate_changes
+        status:=c.ActivateChanges()
+        if status != nil {
+                fmt.Printf("Activate Change got Failed, %s\n", status)
+                return errors.New("Activate Change got Failed, status")
+        }
+        fmt.Printf("Host %s is deleted Successfully \n",host)
+        return nil
 }
 //#-------------------------------------------------------------------------------------------------------------------------------------------
 func (c *Client) ActivateChanges() error {
 	var result StructPutResult
-	resp_body, resp_err := c.NewAPIRequest("POST","activate_changes",nil)
-        if resp_err == nil {
-                err := json.Unmarshal(resp_body, &result)
-                if err == nil {
-                        resp_code := result.ResultCode
-                        if resp_code == 0 {
-                                // now Call activate_changes is successfull
-                                return nil
-                        }
-                }
-                return err
+        resp_body, resp_err := c.NewAPIRequest("POST","activate_changes",nil)
+        if resp_err != nil {
+                fmt.Printf("API Request for activate_changes failed. Error: %s\n", resp_err)
+                return resp_err
         }
-        return resp_err
+        resp_unmarshal_err := json.Unmarshal(resp_body, &result)
+        if resp_unmarshal_err != nil {
+                fmt.Printf("Error Decoding the API response. Error: %s\n", resp_unmarshal_err)
+                return resp_unmarshal_err
+        }
+        resp_code := result.ResultCode
+        if resp_code != 0 {
+                fmt.Printf("API Response Failed. Error:%s\n", result.Result)
+                return errors.New("API Response Failed")
+        }
+        return nil
 }
 //#-------------------------------------------------------------------------------------------------------------------------------------------
--- structs.go
+++ structs.go
@@ -1,40 +1,55 @@
 package cmkapi
 
+//#-------------------------------------------------------------------------------------------------------------------------------------------
 type StructGetHostResult struct {
-	Result struct {
-		Attributes struct {
-			NetworkScan struct {
-				ScanInterval int `json:"scan_interval"`
-				ExcludeRanges []interface{} `json:"exclude_ranges"`
-				IPRanges []interface{} `json:"ip_ranges"`
-				RunAs string `json:"run_as"`
-			} `json:"network_scan"`
-			TagAgent string `json:"tag_agent"`
-			SnmpCommunity string `json:"snmp_community"`
-			Ipv6Address string `json:"ipv6address"`
-			Alias string `json:"alias"`
-			TagCriticality string `json:"tag_criticality"`
-			Site string `json:"site"`
-			TagAddressFamily string `json:"tag_address_family"`
-			Contactgroups []interface{} `json:"contactgroups"`
-			NetworkScanResult struct {
-				Start interface{} `json:"start"`
-				State interface{} `json:"state"`
-				End interface{} `json:"end"`
-				Output string `json:"output"`
-			} `json:"network_scan_result"`
-			Parents []interface{} `json:"parents"`
-			Ipaddress string `json:"ipaddress"`
-			TagNetworking string `json:"tag_networking"`
-		} `json:"attributes"`
-		Hostname string `json:"hostname"`
-		Path string `json:"path"`
-	} `json:"result"`
-	ResultCode int `json:"result_code"`
+        Result struct {
+                Attributes struct {
+                        NetworkScan struct {
+                                ScanInterval int `json:"scan_interval"`
+                                ExcludeRanges []interface{} `json:"exclude_ranges"`
+                                IPRanges []interface{} `json:"ip_ranges"`
+                                RunAs string `json:"run_as"`
+                        } `json:"network_scan"`
+                        TagAgent string `json:"tag_agent"`
+                        SnmpCommunity string `json:"snmp_community"`
+                        Ipv6Address string `json:"ipv6address"`
+                        Alias string `json:"alias"`
+                        TagCriticality string `json:"tag_criticality"`
+                        Site string `json:"site"`
+                        TagAddressFamily string `json:"tag_address_family"`
+                        Contactgroups []interface{} `json:"contactgroups"`
+                        NetworkScanResult struct {
+                                Start interface{} `json:"start"`
+                                State interface{} `json:"state"`
+                                End interface{} `json:"end"`
+                                Output string `json:"output"`
+                        } `json:"network_scan_result"`
+                        Parents []interface{} `json:"parents"`
+                        Ipaddress string `json:"ipaddress"`
+                        TagNetworking string `json:"tag_networking"`
+                } `json:"attributes"`
+                Hostname string `json:"hostname"`
+                Path string `json:"path"`
+        } `json:"result"`
+        ResultCode int `json:"result_code"`
 }
 
-
+//#-------------------------------------------------------------------------------------------------------------------------------------------
 type StructPutResult struct {
-	Result string `json:"result"`
-	ResultCode int `json:"result_code"`
+        Result string `json:"result"`
+        ResultCode int `json:"result_code"`
 }
+//#-------------------------------------------------------------------------------------------------------------------------------------------
+type Attributes struct {
+        Alias string `json:"alias"`
+        TagAgent string `json:"tag_agent"`
+        TagCriticality string `json:"tag_criticality"`
+        Ipaddress string `json:"ipaddress"`
+}
+//#-------------------------------------------------------------------------------------------------------------------------------------------
+type Host struct {
+        Attributes `json:"attributes"`
+        Hostname string `json:"hostname"`
+        Folder string `json:"folder"`
+}
+//#-------------------------------------------------------------------------------------------------------------------------------------------
